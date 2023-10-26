e7883cdb2ff892cfbf9121f8cf80a318c43f59ec
Ben Sabler <bensabler@protonmail.com>
2023-10-26 10:40:21 -0400

    create header and footer (mobile)

---
+++ frontend/src/layout/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer bg-dark py-3 mt-auto text-white">
+    <div class="container d-flex justify-content-center">
+      <span>Ben Sabler &copy;{{ new Date().getFullYear() }} </span>
+    </div>
+  </footer>
+</template>
+
+<script></script>
+
+<style scoped>
+* {
+  border: 1px red solid;
+}
+
+footer {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 15vh;
+  width: 100%;
+}
+
+span {
+  font-family: "Sofia-Pro", sans-serif;
+  font-weight: 300;
+  font-size: 0.9rem;
+  letter-spacing: 0.1rem;
+  color: #fff;
+}
+</style>
--- frontend/src/layout/TheHeader.vue
+++ frontend/src/layout/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar p-3 navbar-expand-lg">
+  <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <span class="brand-one">BEN </span>
@@ -17,39 +17,15 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav folio-link">
+        <ul class="navbar-nav pe-2 pt-4">
           <li class="nav-item">
-            <a class="nav-link" href="#">ABOUT</a>
+            <a class="nav-link ms-auto text-lg-center" href="#">PROJECTS</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">RESUME</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              PROJECTS
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Project 1</a></li>
-              <li><a class="dropdown-item" href="#">Project 2</a></li>
-              <li><a class="dropdown-item" href="#">Project 3</a></li>
-            </ul>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto user-link">
-          <li class="nav-item">
-            <a class="nav-link" href="#">LOGIN</a>
+            <a class="nav-link ms-auto text-lg-center" href="#">PHOTOGRAPHY</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">SIGN UP</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">LOGOUT</a>
+            <a class="nav-link ms-auto text-lg-center" href="#">BLOG</a>
           </li>
         </ul>
       </div>
@@ -64,7 +40,6 @@
   font-family: "Sofia-Pro", sans-serif;
   font-size: 1.4rem;
   letter-spacing: 0.1rem;
-
   color: #000;
 }
 
@@ -79,6 +54,13 @@
 .nav-link:hover,
 .nav-link:focus {
   font-weight: 500;
+}
+
+ul li a {
+  width: 165px;
+  padding: 8px 15px;
+  margin: 0;
+  transition: all 0.5s ease;
 }
 
 .brand-one {
